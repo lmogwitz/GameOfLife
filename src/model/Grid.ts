41bafdde0c56a22c36c617890f1493541cef7cc9
Lars Mogwitz <mogwitz@envi-systems.com>
2022-07-17 15:26:07 +0200

    CellState via booleans

--- src/model/Grid.ts
+++ src/model/Grid.ts
@@ -1,28 +1,20 @@
-class Cell {
-    public isAlive = false;
-
-    public toggle() {
-        this.isAlive = !this.isAlive;
-    }
-}
-
 export class Grid {
-    private readonly _cells: Cell[][];
+    private readonly _cells: boolean[][];
 
     public constructor(public sizeX: number,
                        public sizeY: number) {
         this._cells = new Array(sizeY).fill(null).map((y) => {
             return new Array(sizeX).fill(null).map(x => {
-                return new Cell();
+                return false;
             });
         });
     }
 
-    public get cells(): Cell[][] {
+    public get cells(): boolean[][] {
         return this._cells;
     }
 
     public toggleCell(x: number, y: number): void {
-        this._cells[y][x].toggle();
+        this._cells[y][x] = !this._cells[y][x];
     }
 }
